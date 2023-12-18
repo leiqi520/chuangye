6b0a1136700ec188e02228d0d96c7b04ce770ed9
leiqi <1951668982@qq.com>
2023-12-18 10:37:46 +0800

    基础创建

---
+++ src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<script setup>
+ let username = ''
+ let password = ''
+const onSubmit = (values) => {
+  console.log('submit', values)
+}
+</script>
+<template>
+  <van-nav-bar title="登录" />
+  <van-form @submit="onSubmit">
+    <van-cell-group inset>
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+    </van-cell-group>
+    <div class="item" style="margin: 16px">
+      <van-button round block type="primary" native-type="submit"> 提交 </van-button>
+    </div>
+  </van-form>
+</template>
+<style>
+.item {
+  margin-top: 50px !important;
+}
+</style>
